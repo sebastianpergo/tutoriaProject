21dda5ef263f7018226d8b44f9d788b37efec19f
Sebastian Perdomo <sebastianperdomopg@gmail.com>
2023-03-02 15:12:17 -0500

    Ionic project. Tabs ready and Img loaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,22 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'ticket',
+    loadChildren: () => import('./ticket/ticket.module').then( m => m.TicketPageModule)
+  },
+  {
+    path: 'ajustes',
+    loadChildren: () => import('./ajustes/ajustes.module').then( m => m.AjustesPageModule)
+  },
+  {
+    path: 'cover',
+    loadChildren: () => import('./cover/cover.module').then( m => m.CoverPageModule)
   }
 ];
 @NgModule({
---
+++ src/app/cover/cover.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cover',
+  templateUrl: './cover.page.html',
+  styleUrls: ['./cover.page.scss'],
+})
+export class CoverPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
